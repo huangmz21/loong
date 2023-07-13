9a8326bfa5f8ef90df2926e6440b23ad3dc0ead2
huangmz21 <2799926400@qq.com>
2023-07-13 20:05:53 +0800

    用来写总线部分

---
+++ AXI_CPU/hilo.v
@@ -0,0 +1,23 @@
+module hilo(
+    input        clk,
+    input  [1:0]  hl_we,
+    input  [31:0] h_wdata,
+    input  [31:0] l_wdata,
+    output [31:0] h_rdata,
+    output [31:0] l_rdata
+);
+reg [31:0] hilo[1:0];
+
+always @(posedge clk) begin
+    if(hl_we[1])
+        hilo[1]<=h_wdata;
+    if(hl_we[0])
+        hilo[0]<=l_wdata;
+
+end
+
+assign h_rdata = hilo[1];
+assign l_rdata = hilo[0];
+
+
+endmodule
